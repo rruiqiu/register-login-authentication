79f7f28b656687bd92619026b1f65dcc629aa493
Richard Qiu <>
2022-08-19 22:13:01 -0400

    add env file for security

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
@@ -6,7 +7,6 @@ const port = 3000
 const mongoose = require("mongoose")
 const encrypt = require('mongoose-encryption')
 const app = express()
-
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
@@ -21,8 +21,7 @@ const userSchema = new mongoose.Schema({
 })
 
 
-const secret = "Thisisourlittlesecret"
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] })
+userSchema.plugin(encrypt, { secret: process.env.SECRET_KEY, encryptedFields: ['password'] })
 
 const User = new mongoose.model("User", userSchema)
 
